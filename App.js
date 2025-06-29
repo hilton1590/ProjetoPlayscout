f3503567b614a27d3f1504db016717d87cd91b3d
ryanamumu3 <ryanpipotr@gmail.com>
2025-06-29 14:45:48 -0300

    App.js

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import LoginScreen from './telas/login';
+import Login from './telas/login';
 import Cadastro from './telas/cadastro';
 import MenuPrincipal from './telas/menuPrincipal';
 
@@ -12,9 +12,14 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Login" screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="Cadastro" component={Cadastro} />
         <Stack.Screen name="MenuPrincipal" component={MenuPrincipal} />
+        {/* Futuras telas, só descomentar e criar depois */}
+        {/* <Stack.Screen name="Calendario" component={Calendario} /> */}
+        {/* <Stack.Screen name="Torneios" component={Torneios} /> */}
+        {/* <Stack.Screen name="Notificacoes" component={Notificacoes} /> */}
+        {/* <Stack.Screen name="Estatisticas" component={Estatisticas} /> */}
       </Stack.Navigator>
     </NavigationContainer>
   );

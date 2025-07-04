be20def5cb5112c129fc4a843b7fbe5b9bb133a7
ryanamumu3 <ryanpipotr@gmail.com>
2025-07-03 21:20:28 -0300

    App.js

--- App.js
+++ App.js
@@ -7,7 +7,8 @@ import Cadastro from './telas/cadastro';
 import MenuPrincipal from './telas/menuPrincipal';
 import Notificacoes from './telas/notificacoes';
 import Calendario from './telas/calendario';
-import Torneios from './telas/torneios';  // Importando a tela torneios.js
+import Torneios from './telas/torneios';
+import Noticia from './telas/Noticia';
 
 const Stack = createNativeStackNavigator();
 
@@ -18,11 +19,10 @@ export default function App() {
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="Cadastro" component={Cadastro} />
         <Stack.Screen name="MenuPrincipal" component={MenuPrincipal} />
-        {/* Futuras telas, só descomentar e criar depois */}
         <Stack.Screen name="Calendario" component={Calendario} />
         <Stack.Screen name="Torneios" component={Torneios} />
         <Stack.Screen name="Notificacoes" component={Notificacoes} />
-        {/* <Stack.Screen name="Estatisticas" component={Estatisticas} /> */}
+        <Stack.Screen name="Noticia" component={Noticia} />
       </Stack.Navigator>
     </NavigationContainer>
   );

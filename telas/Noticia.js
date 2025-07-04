4f8e2d70906d24378404f1c18ac76cd7b823efa0
ryanamumu3 <ryanpipotr@gmail.com>
2025-07-03 21:22:26 -0300

    Noticia.js

---
+++ telas/Noticia.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { View, TouchableOpacity, Text, StyleSheet } from 'react-native';
+import { WebView } from 'react-native-webview';
+
+export default function Noticia({ route, navigation }) {
+  const { url } = route.params;
+
+  return (
+    <View style={{ flex: 1 }}>
+      <TouchableOpacity style={styles.closeButton} onPress={() => navigation.goBack()}>
+        <Text style={styles.closeText}>Fechar</Text>
+      </TouchableOpacity>
+      <WebView source={{ uri: url }} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  closeButton: {
+    height: 50,
+    backgroundColor: '#1565c0',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  closeText: {
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+});
